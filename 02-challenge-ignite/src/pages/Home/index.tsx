1a40756b48c4f5741687aee071f55668253b3929
Thiago Negreiros <negreiros.t@gmail.com>
2021-05-13 16:36:53 -0300

    feat: challenge complete

--- 02-challenge-ignite/src/pages/Home/index.tsx
+++ 02-challenge-ignite/src/pages/Home/index.tsx
@@ -25,34 +25,40 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   return sumAmount += 1;
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount = { ...sumAmount };
+    newSumAmount[product.id] = product.amount;
+
+    return newSumAmount;
+
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      await api.get('products')
-                .then(response => setProducts(response.data))
+      const response = await api.get<Product[]>('products');
+
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price)
+      }));
+
+      setProducts(data);
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-   
-
-    
-
-      console.log(findProduct);
+    addProduct(id);
   }
 
   return (
     <ProductList>
       {products.map(product => (
         <li key={product.id}>
-          <img src={product.image} alt="Tênis de Caminhada Leve Confortável" />
+          <img src={product.image} alt={product.title} />
           <strong>{product.title}</strong>
-          <span>{formatPrice(product.price)}</span>
+          <span>{product.priceFormatted}</span>
           <button
               type="button"
               data-testid="add-product-button"
@@ -60,7 +66,7 @@ const Home = (): JSX.Element => {
             >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} 2 */}
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### All projects create at Rocketseat course
+
+https://www.notion.so/Desafio-01-Criando-um-hook-de-carrinho-de-compras-5769216778794019a83f544e79167b12#f2c183bfaa474ac28990340bae730394
