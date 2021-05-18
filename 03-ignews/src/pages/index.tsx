739163adf342ebb8594a6b0d0c9057e405ecbe4f
Thiago Negreiros <negreiros.t@gmail.com>
2021-05-18 14:32:56 -0300

    feat: layout finish

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,14 +1,28 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styled from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início| ig.news</title>
+        <title>Home| ig.news</title>
       </Head>
-      <h1>
-        Hello world
-      </h1>
+      <main className={styled.contentContainer}>
+        <section className={styled.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton/>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Menina codando" />
+      </main>
     </>
   )
 }
