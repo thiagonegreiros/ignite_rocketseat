347a2ccffdc76b84a6d71188d3acf002adb42582
Thiago Negreiros <negreiros.t@gmail.com>
2021-05-24 16:26:40 -0300

    feat: add stripe validations payment

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../pages/api/api';
+import { getStripeJs } from '../../services/stripe-js';
 import style from './styles.module.scss'
 
 interface SubsribeButtonProps {
@@ -5,10 +8,33 @@ interface SubsribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: SubsribeButtonProps) {
+  const [session] = useSession();
+  
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId })
+
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
   return (
     <button
       type="button"
       className={style.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe Now
     </button>
